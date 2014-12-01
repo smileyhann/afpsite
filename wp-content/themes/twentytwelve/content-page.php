3ddf8460b225d6bc75f2b7659f71b4a4d08ab7eb
Hannah Grace <bulataohannahgrace@yahoo.com>
2014-12-01 17:48:13 +0800

    add quick links

--- wp-content/themes/twentytwelve/content-page.php
+++ wp-content/themes/twentytwelve/content-page.php
@@ -15,12 +15,13 @@
 			<?php endif; ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header> -->
-
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
-		<footer class="entry-meta">
+		
+		<footer class="entry-meta" style="margin-bottom:-60px;">
+			<br><br>
 			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
--- wp-content/themes/twentytwelve/footer.php
+++ wp-content/themes/twentytwelve/footer.php
@@ -10,11 +10,43 @@
  */
 ?>
 	</div><!-- #main .wrapper -->
-
+	<div id="footer-sidebar" class="secondary">
+	<h3 style="color:#2f530e;font-size:20px;font-style:italic;margin-bottom:10px;">Quick Links</h3>
+				<div id="footer-sidebar1">
+				<?php
+				if(is_active_sidebar('footer-sidebar-1')){
+				dynamic_sidebar('footer-sidebar-1');
+				}
+				?>
+				</div>
+				<div id="footer-sidebar2">
+				<?php
+				if(is_active_sidebar('footer-sidebar-2')){
+				dynamic_sidebar('footer-sidebar-2');
+				}
+				?>
+				</div>
+				<div id="footer-sidebar3">
+				<?php
+				if(is_active_sidebar('footer-sidebar-3')){
+				dynamic_sidebar('footer-sidebar-3');
+				}
+				?>
+				</div>
+				<div id="footer-sidebar4">
+				<?php
+				if(is_active_sidebar('footer-sidebar-4')){
+				dynamic_sidebar('footer-sidebar-4');
+				}
+				?>
+				</div>
+				</div>
 	<footer id="colophon" role="contentinfo" style="background-color:#b3fcfa;">
 		<div class="site-info">
+			
 			</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
