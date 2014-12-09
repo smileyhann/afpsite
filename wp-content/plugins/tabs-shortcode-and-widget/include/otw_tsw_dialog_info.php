8244022eeca55052514a8bd5c7761005df849e62
Hannah Grace <bulataohannahgrace@yahoo.com>
2014-12-09 16:25:13 +0800

    add facilities page

--- wp-content/plugins/tabs-shortcode-and-widget/include/otw_tsw_dialog_info.php
+++
@@ -1,3 +0,0 @@
-<?php
-$otw_tsw_dialog_text = 'Check out the full version of the <a href="http://otwthemes.com/product/content-manager-for-wordpress/?utm_source=wp.org&amp;utm_medium=admin&amp;utm_content=upgrade&amp;utm_campaign=cml">Content Manager for WordPress</a>.';
-?>
